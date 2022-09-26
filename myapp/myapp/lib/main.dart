d5e33cc106dd52223291956972d422a4acb57054
useonguk <ysu0219@gmail.com>
2022-09-26 21:21:22 +0900

    AlertDialog + ElevatedButton

--- myapp/myapp/lib/main.dart
+++ myapp/myapp/lib/main.dart
@@ -1,72 +1,63 @@
 import 'package:flutter/material.dart';
-import 'package:myapp/widgets/a.dart';
 
 void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  static const String _title = 'Flutter Code Sample';
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: _title,
-      home: MyStatelessWidget()
+      home: Scaffold(
+        appBar: null,
+        body: Center(
+          child: DialogExample(),
+        ),
+      )
     );
   }
 }
 
-
-class MyStatelessWidget extends StatefulWidget {
-  const MyStatelessWidget({Key? key}) : super(key: key);
-
-  @override
-  State<MyStatelessWidget> createState() => _MyStatelessWidgetState();
-}
-
-class _MyStatelessWidgetState extends State<MyStatelessWidget> {
-void showDialog(){
-      AlertDialog(
-        title: Text("hello"),
-      );
-    }
-
+class DialogExample extends StatelessWidget {
+  const DialogExample ({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    
-    return Scaffold(
-      body: Center(
-        child: Column(
-          children:[
-    
-            Image.asset("img/img.jpg"),
-          const Test(),
-            ElevatedButton(
-              style: ElevatedButton.styleFrom(
-        textStyle: const TextStyle(fontSize: 20),
-        shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(10)),
+    return TextButton(
+      child: const Text(
+          'test 버튼',
+          style: TextStyle(
+            color:Colors.orange
+          ),
       ),
-              onPressed: () {
-                showDialog();
-              },
-              child: const Text('새로 시작하기'),
+      onPressed: () => showDialog<String>(
+        context: context,
+        builder:(BuildContext context) => AlertDialog(
+          title: const Text('바늘이'),
+          content: const Text('저는 리엑트를 잘 다룹니다 \n 2021년에 입학했어요'),
+          actions: [
+            ElevatedButton(style : ElevatedButton.styleFrom(
+              textStyle:const TextStyle(
+                fontSize: 20,
+                color : Color(0x9B9B9B),
+              ),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10)
+              ),
+              //ButtonStyle(
+              // backgroundColor: white,
+              //),
             ),
-            ElevatedButton(
-              style: ElevatedButton.styleFrom(
-        textStyle: const TextStyle(fontSize: 20),
-        shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(10)),
-      ),
-              onPressed: () {},
-              child: const Text('로그인'),
-            ),
+              onPressed: () => Navigator.pop(context, 'cancel'), 
+            child: const Text('취소')),
+
+            ElevatedButton(style : ElevatedButton.styleFrom(),
+            onPressed: () => Navigator.pop(context, 'chatting'), 
+            child: const Text('채팅하기'))
           ],
-        ),
-      ),
+        )
+      )
     );
   }
 }
