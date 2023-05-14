90f642d0a2c51747bbb8ade1371c7320b927fe85
useonguk <102145499+useonguk@users.noreply.github.com>
2023-05-14 21:44:36 +0900

    study widget button 라우팅

---
+++ lib/main.dart
@@ -0,0 +1,94 @@
+import 'package:flutter/material.dart';
+import 'main2.dart';
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: QuizStart(),
+    );
+  }
+}
+
+class QuizStart extends StatefulWidget {
+  const QuizStart({super.key});
+
+  @override
+  State<QuizStart> createState() => _QuizStartState();
+}
+
+class _QuizStartState extends State<QuizStart> {
+  String todayWorld = 'baek';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        color: Colors.blue,
+        width: MediaQuery.of(context).size.width,
+        child: Column(
+          children: [
+            SizedBox(
+              height: MediaQuery.of(context).size.height * 0.05,
+            ),
+              Image.asset('images/logo.png'), 
+            SizedBox(
+              height: MediaQuery.of(context).size.height * 0.03,
+            ),
+            const Text('퀴즈 풀러가기',
+              style: TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.w400,
+                fontSize: 32,
+              ),),
+            SizedBox(
+              height: MediaQuery.of(context).size.height * 0.02,
+            ),
+            ElevatedButton(
+              onPressed: (){
+                Navigator.of(context).push(MaterialPageRoute(
+                  builder: (_) => const ChoseQize(),
+                  )
+                );
+              },
+              style: ElevatedButton.styleFrom(
+                elevation: 0,
+              ),
+              child: Image.asset('images/button.png'),
+            ),
+            SizedBox(height: MediaQuery.of(context).size.height * 0.07,),
+            Container(
+              width:MediaQuery.of(context).size.width * 0.8, 
+              height: MediaQuery.of(context).size.height * 0.45,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(30),
+              ),
+              margin: const EdgeInsets.only(bottom: 0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(todayWorld,
+                    style: const TextStyle(
+                      fontSize: 38,
+                      fontWeight: FontWeight.w600,
+                      decoration: TextDecoration.underline,
+                      decorationThickness: 1
+                    ),
+                    textAlign: TextAlign.center,
+                  )
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
---
+++ lib/main2.dart
@@ -0,0 +1,93 @@
+import 'package:flutter/material.dart';
+import 'main.dart';
+
+class ChoseQize extends StatelessWidget {
+  const ChoseQize({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              color: Colors.white,
+              width: MediaQuery.of(context).size.width * 0.8,
+              height: MediaQuery.of(context).size.height * 0.15,
+              child: ElevatedButton(
+                onPressed: (){
+                  Navigator.of(context).push(MaterialPageRoute(
+                    builder: (_) => QuizStart(),
+                    )
+                  );
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.white,
+                  side: const BorderSide(
+                    color: Colors.black,
+                    width: 1,
+                  ),
+                ),
+                child: const Column(
+                  children: [
+                    SizedBox(height: 9,),
+                    Text('맞춤법 퀴즈',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 30,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 14,
+                    ),
+                    Text('단어의 알맞은 답을찾아 맞춤법를 늘리세요',
+                      style: TextStyle(
+                        color: Colors.black,
+                      )
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            SizedBox(
+              height: MediaQuery.of(context).size.height * 0.13,
+            ),
+            Container(
+              color: Colors.white,
+              width: MediaQuery.of(context).size.width * 0.8,
+              height: MediaQuery.of(context).size.height * 0.15,
+              child: ElevatedButton(
+                onPressed: (){},
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.white,
+                  side: const BorderSide(
+                    color: Colors.black,
+                  ),
+                ),
+                child: const Column(
+                  children: [
+                    SizedBox(height: 9,),
+                    Text('어휘 퀴즈',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 30,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 14,
+                    ),
+                    Text('단어의 알맞은 답을찾아 어휘를 늘리세요',
+                      style: TextStyle(
+                        color: Colors.black,
+                      ),)
+                  ],
+                ),
+              ),
+            ), 
+          ],
+        ),
+      ),
+    );
+  }
+}
